91bfa6c1adf705949840b9d936e3d0a4f92b37df
lasherhn <acosta.felipe@gmail.com>
2015-06-05 21:13:18 -0500

    error precision changed

--- R/mcemGLMM.R
+++ R/mcemGLMM.R
@@ -37,7 +37,7 @@ mcemGLMM <- function(fixed, random, data, family = c("bernoulli", "poisson", "ne
   xlabs <- colnames(kX)
   
   # Options
-  ctrl <- list(EMit = 100, MCit = 5000, MCf = 1.03, verb = FALSE, MCsd = NULL, EMdelta = 0.02, EMepsilon = 0.005)
+  ctrl <- list(EMit = 100, MCit = 5000, MCf = 1.03, verb = FALSE, MCsd = NULL, EMdelta = 0.02, EMepsilon = 0.01)
   ctrlN <- names(ctrl)
   ctrl[(controlN <- names(controlEM))] <- controlEM
   if(length(unkwn <- controlN[!controlN %in% ctrlN])){
