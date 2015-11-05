c9530cb074c56dd86d65259351f165b5e9825960
lasherhn <acosta.felipe@gmail.com>
2015-11-05 17:36:55 -0600

    fixed the residuals

--- R/residuals.mcemGLMM.R
+++ R/residuals.mcemGLMM.R
@@ -20,7 +20,7 @@ residuals.mcemGLMM <- function(object, type = c("deviance", "pearson"), ...) {
     }
     if (type[1] == "deviance") {
       # print(mu0)
-      res0 <- sign(object$y - mu0) * sqrt(2 * object$y * log(object$y / mu0) - 2 * (object$y - mu0))
+      res0 <- sign(object$y - mu0) * sqrt(2 * ifelse(object$y > 0, object$y * log(object$y / mu0), 0) - 2 * (object$y - mu0))
     }
   }
   
@@ -31,7 +31,7 @@ residuals.mcemGLMM <- function(object, type = c("deviance", "pearson"), ...) {
       res0 <- (object$y - mu0) / sqrt(mu0 * (1 + 1/a0))
     }
     if (type[1] == "deviance") {
-      res0 <- sign(object$y - mu0) * sqrt(2 * (object$y * log(object$y / mu0)) - (object$y + a0) * log((object$y + a0)/(mu0 + a0)))
+      res0 <- sign(object$y - mu0) * sqrt(2 * (ifelse(object$y > 0, object$y * log(object$y / mu0), 0)) - 2 * (object$y + a0) * log((object$y + a0)/(mu0 + a0)))
     }
   }
   return(as.vector(res0))
